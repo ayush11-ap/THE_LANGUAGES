32d0e1989f67efeda35e2e74f86efdfe0d386373
NITESH-DK <nittesh.dk@gmail.com>
2024-08-15 14:51:01 +0530

    readme ,d,d

--- Java/app1.java
+++ Java/app1.java
@@ -1,19 +1,17 @@
 import java.util.* ;
 
-
-
 public class app1 {
-    public static void main (String args[]){
+  public static void main (String args[]){
+     Scanner scnr = new Scanner(System.in);
 
 
+// Type Casting  ;
 
-// Scanner scnr = new Scanner(System.in);
-
-int lng = 10000 ;
-long inter = lng  ;
-System.out.println(inter); 
-
-
+//  Type Promotion in Expressions
+// 1 . Java automatically promotes each byte, short, or char operand to int
+// when evaluating an expression.
+// 2. If one operand is long, float or double the whole expression is
+// promoted to long, float, or double respectively.
 
 
 
