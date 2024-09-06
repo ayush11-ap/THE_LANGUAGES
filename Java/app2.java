6ba8fbe99ef27b4aaaced7696d1ad4219688c40c
Nitesh-DK <kushwahandk@gmail.com>
2024-09-06 11:53:48 +0530

    day unknown is done

--- Java/app2.java
+++ Java/app2.java
@@ -147,11 +147,9 @@ public static void ButterFly(int n) {
             System.out.print(" ");
         }
 
-       
         for (int j = 0; j < i + 1; j++) {
             System.out.print("*");
         }
-
         System.out.println("");
    
     }
--- javascript/opps/oops.js
+++ javascript/opps/oops.js
@@ -36,15 +36,120 @@
 //*    - Inheritance
 //*    - Polymorphism
 
-// ! Parts of OOP: Language-Specific Features
+// ?  Parts of OOP: Language-Specific Features
 
 
-//?  1. Object Literals
-//?  2. Constructor Functions:
+
+
+//?  1. Object Literals : - 
+
+// let user  = {
+//         name:"akshay",
+//         age:23 ,
+//         cars:{
+//                 name:"ff"
+//         } ,
+//         isLoggedIn :false ,
+//         isUserLoggedIn:function (){
+
+//                 console.log(this.isLoggedIn)
+//         }
+
+// }
+
+// // console.log(user['cars']['name'])
+// // console.log(user.cars.name)
+
+// user.adress = "tc"
+// user.name= "siddhant"
+// // console.log(user)
+
+// delete user.age 
+// // console.log(user)
+// user.isUserLoggedIn();
+
+
+
 //?     - `this` Keyword
 //?       - `this` with different contexts
-        //?    - in broswser , in node , in function , in arrow function , in event listenert ... 
+        //?    - in broswser , in node , in function , in arrow function , in event listenert , in methods ...
+
+
+// in global in browser  -- > window 
+//  in global in node -- > {}
+// in function --> in node --> gloabal node object
+//  in function --> in broswer --> window 
+
+//  in arrow function -- > rememeber , arrow function this always targets the  parent this 
+// in node --> {}
+//  in browser ---> window {}
+
+
+//? in methods 
+// we are getting the object it self 
+// if you are using arrow function  the value of this will be object i..e parent ka this 
+
+//  ?in event listener 
+//  we know event listner dont work in  node 
+// so the value of this will be the event element itself
+
+
+//?  2. Constructor Functions:
+
+
+
+
+
+
+//  imagine a Scenario  where u have a data of students 
+//  the students has  name , rollono , bloodGroup , marks , 
+//  and some methods as getMarks , update makrs 
+ 
+function Students(aame  , rollono  , marks){   
+        this.stdName = aame ;
+        this.rollono = rollono ;
+        this.marks = marks ;
+        // console.log(this )
+        // return this
+
+}
+function Students1(aame  , rollono  , marks){   
+        this.stdName = aame ;
+        this.rollono = rollono ;
+        this.marks = marks ;
+        // console.log(this )
+        // return this
+
+}
+
+
+
+ let student1 = new Students("xyz" , 21 ,100);
+
+ let student2 = new Students1("abc" , 23 ,10);
+ let student3 = new Students("afsdfbc" , 34 ,100);
+
+
+// console.log(student1)
+// console.log(student2)
+// console.log(student3)
+
+// console.log(student3.constructor)
+// console.log(student2.constructor)
+
+//  parent name -- > cousturctor name 
+//  student name --> instance 
+
+//  instance instanceof Counstructor 
+// student1 instanceof Students1
+
+console.log(student1 instanceof Students)
+
+
+
+
 //?     - Constructor Function & Instance
-//?     - Instances (`new` Keyword) & `instanceof` Method, `instance.constructor`
 //?     - Why Use `new` Keyword
 //?     - Definition of Constructor Function
+//?     - Instances (`new` Keyword) & `instanceof`
+//  Method, `instance.constructor`
