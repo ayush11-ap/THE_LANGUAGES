b827440c9fad167da54145fed71da716ac1364ee
Nitesh-DK <nittesh.dk@gmail.com>
2024-09-10 16:05:12 +0530

    all files changes

--- javascript/opps/answers.js
+++ javascript/opps/answers.js
@@ -55,7 +55,41 @@ function Employees(name,position,salary){
     
  }
 
- let emp1 = new Employees("akshay", "manager" , 1338450);
- console.log(emp1);
+//  let emp1 = new Employees("akshay", "manager" , 1338450);
+//  console.log(emp1);
 
 //  emp1.promote(30)
+
+
+function Book(title, author, isAvailable) {
+    this.title = title;
+    this.author = author;
+    this.isAvailable = isAvailable;
+    this.borrowBook = () => {
+        if(this.isAvailable){
+           this.isAvailable = false;
+        console.log(`${this.title} is borrowed`);
+    } else {
+           console.log(`${this.title} is not available`);
+
+       }
+    };
+}
+
+
+let book1 = new Book("harry potter", "j.k.rowling", true);
+let book2 = new Book("1984", "george orwell", true);
+
+console.log(book1)
+console.log(book2)
+// book1.borrowBook();
+
+
+
+
+//  space complexity 
+// sare quesiton ka  time com aur space 
+// revision ye hafte
+
+//  prototype aur inheritance 
+
